0e7bbeb088dd8a72d9b74a440f0f2fa5d1dc25e6
Sajid Chowdhury <msrchk@gmail.com>
2021-10-09 19:55:11 +0600

    added plus icon

--- src/Components/Home/Country/Country.js
+++ src/Components/Home/Country/Country.js
@@ -1,3 +1,5 @@
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Card, Col } from 'react-bootstrap';
 
@@ -9,6 +11,7 @@ const Country = (props) => {
             <Card>
                 <Card.Title>{name.common}</Card.Title>
                 <Card.Img src={flags.png} />
+                <FontAwesomeIcon icon={faPlus} />
             </Card>
         </Col>
     );
